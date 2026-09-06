95a0159a11f1ab0ee3ff6bb40706cdd9e0d0c39c
Meraj Molla <meraj.enigma@gmail.com>
2026-09-06 07:26:24 -0400

    fix incorrect alias

--- cryptocurrency_bot/hedgehog/apps/naive/lib/naive.ex
+++ cryptocurrency_bot/hedgehog/apps/naive/lib/naive.ex
@@ -5,18 +5,16 @@ defmodule Naive do
 
   alias Naive.DynamicSymbolSupervisor
 
-  alias Streamer.DynamicStreamerSupervisor
-
   def start_trading(symbol) do
     symbol
     |> String.upcase()
-    |> DynamicStreamerSupervisor.start_worker()
+    |> DynamicSymbolSupervisor.start_worker()
   end
 
   def stop_trading(symbol) do
     symbol
     |> String.upcase()
-    |> DynamicStreamerSupervisor.stop_worker()
+    |> DynamicSymbolSupervisor.stop_worker()
   end
 
   defdelegate shutdown_trading(symbol), to: DynamicSymbolSupervisor
