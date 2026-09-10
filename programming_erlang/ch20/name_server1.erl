4df2e45bf51616a04d6e59b9e9b40aec496dbbcf
Meraj Molla <meraj.enigma@gmail.com>
2026-09-10 06:26:55 -0400

    name server

---
+++ programming_erlang/ch20/name_server1.erl
@@ -0,0 +1,13 @@
+-module(name_server1).
+-export([add/2, find/1, init/0, handle/2]).
+-import(server1, [rpc/2]).
+
+%% Client routines
+add(Name, Place) -> rpc(?MODULE, {add, Name, Place}).
+find(Name) -> rpc(?MODULE, {find, Name}).
+
+%% Callbacks
+init() -> dict:new().
+
+handle({add, Name, Place}, Dict) -> {ok, dict:store(Name, Place, Dict)};
+handle({find, Name}, Dict) -> {dict:find(Name, Dict), Dict}.
