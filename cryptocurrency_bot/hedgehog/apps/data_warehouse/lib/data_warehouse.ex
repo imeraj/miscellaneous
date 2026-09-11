6779a37111f734710bade953cba220626c1b2595
Meraj Molla <meraj.enigma@gmail.com>
2026-09-11 10:04:28 -0400

    backtesting

--- cryptocurrency_bot/hedgehog/apps/data_warehouse/lib/data_warehouse.ex
+++ cryptocurrency_bot/hedgehog/apps/data_warehouse/lib/data_warehouse.ex
@@ -1,6 +1,8 @@
 defmodule DataWarehouse do
   alias DataWarehouse.Subscriber.DynamicSupervisor
 
+  def publish_data(args), do: DataWarehouse.Publisher.start_link(args)
+
   def start_storing(stream, symbol) do
     to_topic(stream, symbol)
     |> DynamicSupervisor.start_worker()
