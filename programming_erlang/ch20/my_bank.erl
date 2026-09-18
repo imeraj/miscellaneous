fb4d05b553be4bdea877005070beee3ada098d2b
Meraj Molla <meraj.enigma@gmail.com>
2026-09-18 09:17:44 -0400

    terminate

--- programming_erlang/ch20/my_bank.erl
+++ programming_erlang/ch20/my_bank.erl
@@ -52,5 +52,5 @@ handle_call(stop, _From, Tab) ->
     {stop, normal, stopped, Tab}.
 handle_cast(_Msg, State) -> {noreply, State}.
 handle_info(_Info, State) -> {noreply, State}.
-terminate(_Reason, _State) -> ok.
+terminate(normal, _State) -> ok.
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
