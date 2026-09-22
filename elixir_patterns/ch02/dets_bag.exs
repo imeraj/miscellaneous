5faf645a987983bf56b2a9a76eda9939a7a77649
Meraj Molla <meraj.enigma@gmail.com>
2026-09-22 10:02:09 -0400

    format

--- elixir_patterns/ch02/dets_bag.exs
+++ elixir_patterns/ch02/dets_bag.exs
@@ -1,9 +1,7 @@
 {:ok, my_dets_table} = :dets.open_file(~c'my_dets_table.dets', type: :bag)
-
 my_ets_table = :ets.new(:my_metrics_table, [:bag])
 
 :dets.to_ets(my_dets_table, my_ets_table)
-
 :ets.tab2list(my_ets_table) |> IO.inspect()
 
 :ets.insert(my_ets_table, {:auth_attempt, %{user: "Alex", ts: NaiveDateTime.utc_now()}})
@@ -15,7 +13,6 @@ my_ets_table = :ets.new(:my_metrics_table, [:bag])
 )
 
 :ets.to_dets(my_ets_table, my_dets_table)
-
 :dets.info(my_dets_table) |> IO.inspect()
 
 :dets.sync(my_dets_table)
